9d9fcb23f4e56110e1ce0b3eb46da1dd30ce6de5
raphael-khan <raphaelkhan@yahoo.com>
2022-03-24 22:12:17 -0400

    initialized mobilenet model

---
+++ brain.py
@@ -0,0 +1,14 @@
+from imageai.Classification import ImageClassification
+import os
+
+execution_path = os.getcwd()
+
+prediction = ImagePredcition()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(execution_path, 'MobileNetV2'))
+prediction.loadModel()
+
+predictions, probabilities = prediction.predictImage(os.path.join(execution_path, 'godzilla.jpg'), result_count=5)
+
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction, " : ", eachProbability)
